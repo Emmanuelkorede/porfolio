0af65ae3e7e3632f79158816071927aed79ecdba
Emmanuelkorede <emmanueljob2009@gmail.com>
2026-09-14 09:59:36 +0100

    fix : adjusted the imaes for the navdata to match what they meant

--- src/data/navData.ts
+++ src/data/navData.ts
@@ -1,9 +1,10 @@
-import { Home, User, LayoutGrid, Briefcase, Cpu, Send } from 'lucide-react';
+import { Home, User, Wrench, FolderGit2, Briefcase, Cpu, Send } from 'lucide-react';
 
 export const navLinks = [
   { name: 'Home', href: '/', icon: Home },
   { name: 'About', href: '#about', icon: User },
-  { name: 'Projects', href: '#projects', icon: LayoutGrid },
+  { name: 'Services', href: '#services', icon: Wrench },
+  { name: 'Projects', href: '#projects', icon: FolderGit2 },
   { name: 'Journey', href: '#journey', icon: Briefcase },
   { name: 'Skills', href: '#skill', icon: Cpu },
   { name: 'Contact', href: '#contact', icon: Send },
